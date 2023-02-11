c6db0ee667a1016313954af442c0a49b4c4f76df
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-02-11 08:51:39 +0100

    unicafe step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,19 +9,25 @@ const Button = (props) => {
   );
 }
 
-const Statistics = () => <h2>statistics</h2>;
+const StatHeader = () => <h2>statistics</h2>;
 
 const StatLine = (props) => (
   <div>
-    {props.text} {props.count}
+    {props.text} {props.value}
   </div>
 );
+
 
 const App = () => {
   // save click of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const total = good + bad + neutral;
+  const score = good - bad;
+  const avg = total === 0 ? 0 : score / total;
+  const pos = total === 0 ? 0 : (good / total) * 100;
 
   const handleGoodClick = () => {
     console.log("good");
@@ -44,10 +50,13 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good' />
       <Button handleClick={handleNeutralClick} text='neutral' />
       <Button handleClick={handleBadClick} text='bad' />
-      <Statistics />
-      <StatLine text='good' count={good} />
-      <StatLine text='neutral' count={neutral} />
-      <StatLine text='bad' count={bad} />
+      <StatHeader />
+      <StatLine text='good' value={good} />
+      <StatLine text='neutral' value={neutral} />
+      <StatLine text='bad' value={bad} />
+      <StatLine text='all' value={total} />
+      <StatLine text='average' value={avg} />
+      <StatLine text='positive' value={`${pos} %`} />
     </div>
   )
 }
