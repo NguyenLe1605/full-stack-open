43485327174466d6d78f665dd6b30dd1abd5f3b1
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-03-13 14:52:39 +0700

    Refactor controller and models

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,15 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogRouter = require('./controllers/blogs');
 
 const mongoUrl = 'mongodb+srv://fullstack:nPw1T9T5RxAsErbJ@cluster0.larvaqr.mongodb.net/bloglist?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
@@ -18,23 +10,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
