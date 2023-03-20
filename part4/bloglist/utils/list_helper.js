050b038f2171da4b4ec5ff378245b2ceda9f2f2c
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-03-20 13:15:46 +0700

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) =>  {
+    if (blogs == null) return 0;
+    return blogs.reduce((acc, curr) => acc + curr.likes, 0); 
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
