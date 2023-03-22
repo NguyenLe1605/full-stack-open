f92611acf3826e22af14ba4bd44322e15a759ce5
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-03-22 15:17:56 +0700

    Exercise 4.9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,10 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+})
   
 blogRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
@@ -18,5 +15,6 @@ blogRouter.post('/', (request, response) => {
         response.status(201).json(result)
         })
 })
+
 
 module.exports = blogRouter;
