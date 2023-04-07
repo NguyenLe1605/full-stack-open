9b9cc80203eedcfeed26ab5ab56c1710a819b481
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-04-07 19:41:47 +0700

    Exercise 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const { errorHandler } = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const morgan = require('morgan')
 
 mongoose.set('strictQuery', false)
@@ -18,11 +18,12 @@ app.use(express.json())
 
 morgan.token('data', (request) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
-app.use(errorHandler)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,15 +4,6 @@ const User = require('../models/user')
 
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = request => {  
-	const authorization = request.get('authorization')  
-	if (authorization && authorization.startsWith('Bearer ')) {    
-		return authorization.replace('Bearer ', '')  
-	}  
-	return null
-}
-
-
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {blogs: 0});
   response.json(blogs);
@@ -20,8 +11,7 @@ blogRouter.get('/', async (request, response) => {
   
 blogRouter.post('/', async (request, response) => {
     const body = request.body;
-    const token = getTokenFrom(request);
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)  
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)  
     if (!decodedToken.id) {    
       return response.status(401).json({ error: 'token invalid' })  
     } 
