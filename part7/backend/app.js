34e52362b24eab1ab6d37dae20bc7450ec2f42ee
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-05-16 07:34:54 +0700

    Exercise 7.10

---
+++ part7/backend/app.js
@@ -0,0 +1,36 @@
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware");
+const morgan = require("morgan");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+morgan.token("data", (request) => JSON.stringify(request.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+app.use(middleware.tokenExtractor);
+
+app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ part7/backend/utils/list_helper.js
@@ -0,0 +1,55 @@
+const _ = require("lodash");
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  if (blogs == null) return 0;
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs == null || blogs.length === 0) return null;
+  const { title, author, likes } = blogs.reduce((acc, curr) =>
+    acc.likes > curr.likes ? acc : curr
+  );
+  return { title, author, likes };
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs == null || blogs.length === 0) {
+    return null;
+  }
+
+  const groupedBlogs = _.countBy(blogs, "author");
+  const res = Object.keys(groupedBlogs).map((key) => {
+    return {
+      author: key,
+      blogs: groupedBlogs[key],
+    };
+  });
+  return res.reduce((acc, curr) => (acc.blogs > curr.blogs ? acc : curr));
+};
+
+const mostLikes = (blogs) => {
+  if (blogs == null || blogs.length === 0) {
+    return null;
+  }
+
+  const authors = _.groupBy(blogs, "author");
+  const res = Object.keys(authors).map((key) => {
+    return {
+      author: key,
+      likes: _.sumBy(authors[key], "likes"),
+    };
+  });
+  return res.reduce((acc, curr) => (acc.likes > curr.likes ? acc : curr));
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
